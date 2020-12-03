efcfceeb09ef18ee8f196ceb1bd994cf986c187f
Jack <eljukna@gmail.com>
2020-12-03 10:38:31 +0200

    init

---
+++ src/main/java/ups/papersoda/cconvert/CconvertApplication.java
@@ -0,0 +1,11 @@
+package ups.papersoda.cconvert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CconvertApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CconvertApplication.class, args);
+    }
+}
---
+++ src/test/java/ups/papersoda/cconvert/CconvertApplicationTests.java
@@ -0,0 +1,13 @@
+package ups.papersoda.cconvert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CconvertApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
